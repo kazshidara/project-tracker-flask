f37bf9bea2a95b39f054612dc1a78fcfa120b671
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-31 12:56:37 -0700

    Altered hackbright_web.py to receive input from user and implemented HTML pages

--- hackbright_web.py
+++ hackbright_web.py
@@ -11,12 +11,21 @@ app = Flask(__name__)
 def get_student():
     """Show information about a student."""
 
-    github = "jhacks"
+    github = request.args.get('github')
 
     first, last, github = hackbright.get_student_by_github(github)
 
-    return "{} is the GitHub account for {} {}".format(github, first, last)
+    html = render_template("student_info.html", first=first, last=last, github=github)
+    return html
 
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+# @app.route("")
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
     app.run(debug=True)
