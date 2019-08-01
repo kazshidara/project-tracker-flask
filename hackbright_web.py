e523f9488867fb9d4573e4387f907a616a03f106
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-31 17:27:07 -0700

    Added 2 new routes and templates where user can add a new student into the hackbright database

--- hackbright_web.py
+++ hackbright_web.py
@@ -25,7 +25,31 @@ def get_student_form():
 
     return render_template("student_search.html")
 
-# @app.route("")
+
+
+@app.route("/add-student")  #arbitrary route that gets user here if they want to add a new student 
+def added_to_db():
+    """Show New Student Form"""
+
+    return render_template("new_student.html")
+
+
+@app.route("/student-add", methods=['POST'])
+def student_add():
+    """Add a new student form and handle the form results and confirms new student was added."""
+   
+    first_n = request.form.get('first_name')
+    last_n = request.form.get('last_name')
+    github_n = request.form.get('github')
+    hackbright.make_new_student(first_n, last_n, github_n)
+    return render_template("student_added.html", first=first_n, last=last_n, github=github_n)
+   
+
+
+
+
+
+
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
     app.run(debug=True)
